b64780a7c040b90fb6dd033855adc67840fe5b75
DIJO JOSEPH <dijotcr222@gmail.com>
2017-06-22 17:44:59 +1000

    Testing

--- messages/index.js
+++ messages/index.js
@@ -19,22 +19,47 @@ bot.localePath(path.join(__dirname, './locale'));
 // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
 var bot = new builder.UniversalBot(connector, [
     function (session) {
+
         session.send("Welcome to the KiwiSaver.");
+        session.dialogData.partySize = results.response;
         session.send("Hi Dijo.  We think that you’d best suit a KiwiSaver Balanced fund but it’s possible you’d prefer an alternative fund.  What would you like to do?");
     },
     function (session, results) {
-        builder.Prompts.text(session, "So the Balanced fund offers a medium risk, medium return at an average management fee. For a person with your income and age, you should be looking at lowering your risk profile and preparing for retirement.");
-    },
+
+        builder.send("So the Balanced fund offers a medium risk, medium return at an average management fee. For a person with your income and age, you should be looking at lowering your risk profile and preparing for retirement.");
+
+     },
     function (session, results) {
+
         session.dialogData.partySize = results.response;
         builder.Prompts.text(session, "DISPLAY GRAPH. Let's say 7%.");
+
+    },
+     function (session, results) {
+
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "DISPLAY GRAPH. Let's say 7%.");
+
+    },
+     function (session, results) {
+
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "DISPLAY GRAPH. Let's say 8%.");
+
+    },
+     function (session, results) {
+
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "DISPLAY GRAPH. Let's say 9%.");
+
     },
     function (session, results) {
-        session.dialogData.reservationName = results.response;
 
+        session.dialogData.reservationName = results.response;
         // Process request and display reservation details
         session.send("");
         session.endDialog();
+
     }
 ]);
 
