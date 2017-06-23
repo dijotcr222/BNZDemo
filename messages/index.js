10939d8265f2e05a4f28f8e628a941b4f66e6c64
DIJO JOSEPH <dijotcr222@gmail.com>
2017-06-23 12:18:23 +1000

    Cards added

--- messages/index.js
+++ messages/index.js
@@ -38,7 +38,18 @@ bot.localePath(path.join(__dirname, './locale'));
 bot.dialog('/', function (session) {
     session.send('You said ' + session.message.text);
 });
-
+function createHeroCard(session) {
+    return new builder.HeroCard(session)
+        .title('BotFramework Hero Card')
+        .subtitle('Your bots — wherever your users are talking')
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .images([
+            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+        ])
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework', 'Get Started')
+        ]);
+}
 
 if (useEmulator) {
     var restify = require('restify');
